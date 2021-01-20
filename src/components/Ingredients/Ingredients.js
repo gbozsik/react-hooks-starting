aef7671a030cff2acf969f6ff60d1a0ba8b64434
Gabor <g.bozsik@gmail.com>
2021-01-20 21:58:17 +0100

    Lecture 15. What`s useCallBack()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList'
 
 function Ingredients() {
+  const [ingredientsState, setIngredientsState] = useState([])
+
+  const filterIngredientsHandler = useCallback(filteredIngredients => {
+    setIngredientsState(filteredIngredients)
+  }, [])
+
+  const onAddIngredientHandler = (ingredient) => {
+    fetch('https://react-hooks-update-adee4-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify({ ...ingredient }),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(responseJson => {
+      return responseJson.json()    // convert from json to js code
+    }).then(responseData => {
+      setIngredientsState(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ])
+    })
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={onAddIngredientHandler} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadingredients={filterIngredientsHandler}/>
+        <IngredientList ingredients={ingredientsState} onRemoveItem={() => { }} />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadingredients } = props
+  const [enteredFilter, setEnteredFilter] = useState('')
+
+  useEffect(() => {
+    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch('https://react-hooks-update-adee4-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query)
+      .then(response => response.json())    // convert from json to js code
+      .then(responseData => {
+        const loadedIngredients = []
+        for (const key in responseData) {
+          console.log('responseData', responseData)
+          console.log(responseData[key].title)
+          console.log(responseData[key].amount)
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+        onLoadingredients(loadedIngredients)
+      })
+  }, [enteredFilter, onLoadingredients])
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" 
+          value={enteredFilter}
+          onChange={event => setEnteredFilter(event.target.value)}/>
         </div>
       </Card>
     </section>
