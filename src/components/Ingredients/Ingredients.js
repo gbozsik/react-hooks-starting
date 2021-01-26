4df71138885d3b1b705e7fd93a6a74f9cfb7eb94
gbozsik <g.bozsik@gmail.com>
2021-01-26 12:23:14 +0100

    useContext()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,38 +1,108 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useReducer, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList'
+import ErrorModal from '../UI/ErrorModal'
+
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('Should not get here')
+  }
+}
+
+const httpReducer = (currenthttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null }
+    case 'RESPONSE':
+      return { ...currenthttpState, loading: false }  // ...httpState is to copy everythig from the previous state to not loose antyhing and just override the "loading" prop
+    case 'ERROR':
+      return { error: action.errorData, loading: false }
+    case 'CLEAR':
+      return { ...currenthttpState, error: null }
+    default:
+      throw new Error('should not get here')
+  }
+}
 
 function Ingredients() {
-  const [ingredientsState, setIngredientsState] = useState([])
+  const [ingredientsState, dispatch] = useReducer(ingredientReducer, [])
+  const [httpState, dispatchHttp] = useReducer(httpReducer, [])
+  // const [ingredientsState, setIngredientsState] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState()
 
   const filterIngredientsHandler = useCallback(filteredIngredients => {
-    setIngredientsState(filteredIngredients)
+    // setIngredientsState(filteredIngredients)
+    dispatch({ type: 'SET', ingredients: filteredIngredients })
   }, [])
 
   const onAddIngredientHandler = (ingredient) => {
+    // setIsLoading(true)
+    dispatchHttp({ type: 'SEND' })
     fetch('https://react-hooks-update-adee4-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
       method: 'POST',
       body: JSON.stringify({ ...ingredient }),
       headers: { 'Content-Type': 'application/json' }
     }).then(responseJson => {
+      // setIsLoading(false)
+      dispatchHttp({ type: 'RESPONSE', })
       return responseJson.json()    // convert from json to js code
     }).then(responseData => {
-      setIngredientsState(prevIngredients => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient }
-      ])
+      // setIngredientsState(prevIngredients => [
+      //   ...prevIngredients,
+      //   { id: responseData.name, ...ingredient }
+      // ])
+      dispatch({ type: 'ADD', ingredient: { id: responseData.name, ...ingredient } })
+    }).catch(error => {
+      dispatchHttp({ type: 'ERROR', errorData: error.message })
+      // setError(error.message)
+      // setIsLoading(false)
+
     })
+  }
+
+  const onRemoveIngredientHandler = (ingredientId) => {
+    // setIsLoading(true)
+    dispatchHttp({ type: 'SEND' })
+    fetch(`https://react-hooks-update-adee4-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`, {
+      method: 'DELETE',
+    }).then(responseData => {
+      // setIsLoading(false)
+      // setIngredientsState(prevIngredients =>
+      //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+      // );
+      dispatchHttp({ type: 'RESPONSE', })
+      dispatch({ type: 'DELETE', id: ingredientId })
+    }).catch(error => {
+      dispatchHttp({ type: 'ERROR', errorData: error.message })
+      // setError(error.message)
+      // setIsLoading(false)
+    })
+  }
+
+  const clearError = () => {
+    // setError(null)
+    dispatchHttp({type: 'CLEAR'})
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={onAddIngredientHandler} />
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+
+      <IngredientForm onAddIngredient={onAddIngredientHandler} loading={httpState.loading} />
 
       <section>
-        <Search onLoadingredients={filterIngredientsHandler}/>
-        <IngredientList ingredients={ingredientsState} onRemoveItem={() => { }} />
+        <Search onLoadingredients={filterIngredientsHandler} />
+        <IngredientList ingredients={ingredientsState} onRemoveItem={(onRemoveIngredientHandler)} />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,35 +6,40 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadingredients } = props
   const [enteredFilter, setEnteredFilter] = useState('')
+  const inputRef = useRef()
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-update-adee4-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query)
-      .then(response => response.json())    // convert from json to js code
-      .then(responseData => {
-        const loadedIngredients = []
-        for (const key in responseData) {
-          console.log('responseData', responseData)
-          console.log(responseData[key].title)
-          console.log(responseData[key].amount)
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-update-adee4-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query)
+          .then(response => response.json())    // convert from json to js code
+          .then(responseData => {
+            const loadedIngredients = []
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              })
+            }
+            onLoadingredients(loadedIngredients)
           })
-        }
-        onLoadingredients(loadedIngredients)
-      })
-  }, [enteredFilter, onLoadingredients])
+      }
+    }, 500)
+    return () => clearTimeout(timer)
+
+  }, [enteredFilter, onLoadingredients, inputRef])
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" 
-          value={enteredFilter}
-          onChange={event => setEnteredFilter(event.target.value)}/>
+          <input type="text"
+            ref={inputRef}
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)} />
         </div>
       </Card>
     </section>
